62b5a878149f9af71cd141650eba54bde9d88e68
njehh <anssimyk@gmail.com>
2023-11-28 08:45:36 +0200

    Week 4 data script

---
+++ Data/create_human.R
@@ -0,0 +1,10 @@
+
+
+#2. Read in the “Human development” and “Gender inequality” data sets
+
+
+library(readr)
+hd <- read_csv("https://raw.githubusercontent.com/KimmoVehkalahti/Helsinki-Open-Data-Science/master/datasets/human_development.csv")
+gii <- read_csv("https://raw.githubusercontent.com/KimmoVehkalahti/Helsinki-Open-Data-Science/master/datasets/gender_inequality.csv", na = "..")
+
+
